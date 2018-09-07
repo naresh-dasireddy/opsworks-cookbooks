f21b070baddbe58876147ff12ba209a3112507ee
naresh <naresh.d@mtwlabs.com>
2018-09-07 15:33:14 +0530

    changed s3 bucket

--- charvee_test/recipes/artifact.rb
+++ charvee_test/recipes/artifact.rb
@@ -1,7 +1,7 @@
 remote_file 'test2.jar' do
   source lazy {
-    git_describe = shell_out!('git describe --abbrev=0 --tags', cwd: 'node-jenkins-pipeline-test').stdout.strip
-     "https://s3.amazonaws.com/versiontags/#{git_describe}"
+    git_describe = shell_out!('git describe --abbrev=0 --tags', cwd: 'jenkins').stdout.strip
+     "https://s3.amazonaws.com/naresh-mtw/#{git_describe}"
      #"https://s3.amazonaws.com/versiontags/#{git_describe}.jar"
   }
 end
