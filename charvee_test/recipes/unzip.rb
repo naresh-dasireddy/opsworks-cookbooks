ae3cbfe8737346742123f8c19b8adcf09791addd
naresh <naresh.d@mtwlabs.com>
2018-09-07 16:12:31 +0530

    changed s3 bucket

--- charvee_test/recipes/unzip.rb
+++ charvee_test/recipes/unzip.rb
@@ -3,10 +3,10 @@
 #  command "sudo mkdir artifact"
 #end
 
-execute "copy the artifact " do
-  cwd ""
-  command "sudo cp test2.jar project "
-end
+#execute "copy the artifact " do
+ # cwd ""
+ # command "sudo cp test2.jar project "
+#end
 
 execute "unzip the artifact" do
   cwd "project"
